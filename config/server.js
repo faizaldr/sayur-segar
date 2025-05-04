35f76be4617b5d252875d253637bd66170a85233
Faizal Damar Ramadhan <faizal@inixjogja.onmicrosoft.com>
2025-05-04 19:54:52 +0700

    5 commit

--- config/server.js
+++ config/server.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', process.env.APP_KEYS.split(',')),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
