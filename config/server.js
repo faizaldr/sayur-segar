bca763370d73ca8e5f1baef90c35fed761fc4a8b
Faizal Damar Ramadhan <faizal@inixjogja.onmicrosoft.com>
2025-05-04 20:44:22 +0700

    n commit

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 8080),
   app: {
     keys: env.array('APP_KEYS', process.env.APP_KEYS.split(',')),
   },
