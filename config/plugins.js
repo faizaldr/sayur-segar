dc3ee23e0f87a40c8a02f9c076138454c11a8fd3
faizaldr <fdr00zenc@gmail.com>
2025-12-11 14:29:21 +0700

    ok

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,12 @@
-module.exports = () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'firebase-upload',
+      providerOptions: {
+        serviceAccount: env.json('GOOGLE_SERVICE_ACCOUNT'),
+        bucket: env('FIREBASE_BUCKET'),
+        prefix: env('FIREBASE_PREFIX', ''), // optional
+      },
+    },
+  },
+});
