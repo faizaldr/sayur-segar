502d459a88e27e9ec7bb8076276255703bd9448c
faizaldr <fdr00zenc@gmail.com>
2025-12-21 12:00:18 +0700

    ok fix

--- config/plugins.js
+++ config/plugins.js
@@ -1,12 +1,12 @@
 export default ({ env }) => ({
-  upload: {
-    config: {
-      provider: 'firebase-upload',
-      providerOptions: {
-        serviceAccount: env.json('GOOGLE_SERVICE_ACCOUNT'),
-        bucket: env('FIREBASE_BUCKET'),
-        prefix: env('FIREBASE_PREFIX', ''), // optional
-      },
-    },
-  },
+  // upload: {
+  //   config: {
+  //     provider: 'firebase-upload',
+  //     providerOptions: {
+  //       serviceAccount: env.json('GOOGLE_SERVICE_ACCOUNT'),
+  //       bucket: env('FIREBASE_BUCKET'),
+  //       prefix: env('FIREBASE_PREFIX', ''), // optional
+  //     },
+  //   },
+  // },
 });
